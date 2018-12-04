fa76bef5b1a551252d0a369675fe78708e525ee7
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2018-12-04 15:53:19 +0100

    Localize user data

--- source/php/Module/UserAccount/UserAccount.php
+++ source/php/Module/UserAccount/UserAccount.php
@@ -21,6 +21,28 @@ class UserAccount extends \Modularity\Module
         return $data;
     }
 
+    public function getUserData() : array
+    {
+        if (!is_user_logged_in()) {
+            return array();
+        }
+
+        $data = array(
+            'id'             => get_current_user_id(),
+            'email'          => wp_get_current_user()->data->user_email,
+            'firstName'      => get_user_meta(get_current_user_id())['first_name'][0],
+            'lastName'       => get_user_meta(get_current_user_id())['last_name'][0],
+            'company'        => '',
+            'companyNumber'  => '',
+            'phone'          => '',
+            'website'        => '',
+            'billingAddress' => '',
+            'contactPerson'  => ''
+        );
+
+        return $data;
+    }
+
     public function script()
     {
         if (file_exists(MODULARITYRESOURCEBOOKING_PATH . '/dist/' . \ModularityResourceBooking\Helper\CacheBust::name('js/RegistrationForm/Index.js'))) {
@@ -33,7 +55,8 @@ class UserAccount extends \Modularity\Module
             //Localize
             wp_localize_script('modularity-' . $this->slug, 'modUserAccount', array(
                 'translation' => array(),
-                'restUrl' => get_rest_url()
+                'restUrl' => get_rest_url(),
+                'user' => $this->getUserData()
             ));
         }
     }
