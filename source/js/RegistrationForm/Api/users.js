c157fe8a36230463575b3f8649c6e270563da7ca
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2018-12-04 09:52:10 +0100

    Add method for creating user account

---
+++ source/js/RegistrationForm/Api/users.js
@@ -0,0 +1,36 @@
+//Localized var
+const {translation, restUrl} = modRegistrationForm;
+
+const createUser = (user) => {
+    let url = restUrl + 'ModularityResourceBooking/v1/CreateUser';
+    let formData = new FormData();
+
+    const {email, firstName, lastName, company, companyNumber} = user;
+
+    formData.append('email', email);
+    formData.append('company', company);
+    formData.append('first_name', firstName);
+    formData.append('last_name', lastName);
+    formData.append('corporate_number', companyNumber);
+
+    let options = {
+        method: 'POST',
+        body: formData
+    };
+
+    return fetch(url, options)
+    .then((response) => {
+        return response.json();
+    })
+    .then((response) => {
+        if (response.state == 'error') {
+            throw new Error(response.message);
+        }
+
+        return response;
+    });
+};
+
+module.exports = {
+    createUser: createUser
+};
