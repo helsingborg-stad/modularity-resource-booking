2fe133b07738094c62d283f2fe1561598e3ae128
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2018-12-05 14:41:52 +0100

    Add user billing info fields

---
+++ source/acf-fields/php/mod-billing-info.php
@@ -0,0 +1,105 @@
+<?php 
+
+if (function_exists('acf_add_local_field_group')) {
+    acf_add_local_field_group(array(
+    'key' => 'group_5c010376c78be',
+    'title' => __('Billing info', 'modularity-resource-booking'),
+    'fields' => array(
+        0 => array(
+            'key' => 'field_5c010448de6c7',
+            'label' => __('Company name', 'modularity-resource-booking'),
+            'name' => 'billing_company',
+            'type' => 'text',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'default_value' => '',
+            'placeholder' => '',
+            'prepend' => '',
+            'append' => '',
+            'maxlength' => '',
+        ),
+        1 => array(
+            'key' => 'field_5c01038bde6c4',
+            'label' => __('Company number', 'modularity-resource-booking'),
+            'name' => 'billing_company_number',
+            'type' => 'number',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'default_value' => '',
+            'placeholder' => '',
+            'prepend' => '',
+            'append' => '',
+            'min' => '',
+            'max' => '',
+            'step' => '',
+        ),
+        2 => array(
+            'key' => 'field_5c010402de6c6',
+            'label' => __('Contact person', 'modularity-resource-booking'),
+            'name' => 'billing_contact_person',
+            'type' => 'text',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'default_value' => '',
+            'placeholder' => '',
+            'prepend' => '',
+            'append' => '',
+            'maxlength' => '',
+        ),
+        3 => array(
+            'key' => 'field_5c0103dfde6c5',
+            'label' => __('Billing address', 'modularity-resource-booking'),
+            'name' => 'billing_address',
+            'type' => 'textarea',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'default_value' => '',
+            'placeholder' => '',
+            'maxlength' => '',
+            'rows' => '',
+            'new_lines' => '',
+        ),
+    ),
+    'location' => array(
+        0 => array(
+            0 => array(
+                'param' => 'user_role',
+                'operator' => '==',
+                'value' => 'all',
+            ),
+        ),
+    ),
+    'menu_order' => 0,
+    'position' => 'normal',
+    'style' => 'default',
+    'label_placement' => 'top',
+    'instruction_placement' => 'label',
+    'hide_on_screen' => '',
+    'active' => 1,
+    'description' => '',
+));
+}
