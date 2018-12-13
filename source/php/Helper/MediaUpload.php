ebb0b897b876484bd8f7b579ceb60ed273fc97cd
Carl Johan Silvergrund <johan.silvergrund@hiq.se>
2018-12-13 13:23:06 +0100

    format

--- source/php/Helper/MediaUpload.php
+++ source/php/Helper/MediaUpload.php
@@ -6,34 +6,26 @@ class MediaUpload
 {
     public static function upload($uploads, $mediaIds = array())
     {
-
         //Require resources
         require_once(ABSPATH . 'wp-admin/includes/image.php');
         require_once(ABSPATH . 'wp-admin/includes/file.php');
         require_once(ABSPATH . 'wp-admin/includes/media.php');
-
         //Check if have file uploads
         if (!is_array($uploads) || empty($uploads)) {
             return;
         }
-
         if (is_array($uploads) && !empty($uploads)) {
-
             foreach ($uploads as $upload) {
-
                 //Not a valid upload for some reason, move to next
                 if (self::checkUploadErrors($upload) === false) {
                     continue;
                 }
-
                 //Check if a valid mime type
                 if (self::checkMimeType($upload) !== true) {
                     continue;
                 }
-
                 //Move to correct location
                 $fileData = \wp_handle_upload($upload, array('test_form' => false));
-
                 //Enter to WordPress media library
                 if (is_array($fileData) && !empty($fileData) && isset($fileData['file'])) {
                     $mediaIds[] = \wp_insert_attachment(
@@ -46,12 +38,12 @@ class MediaUpload
                 }
             }
         }
-
         //Return id's of uploaded media files
         return array_filter($mediaIds);
     }
 
-    public static function checkUploadErrors($file) {
+    public static function checkUploadErrors($file)
+    {
         switch ($file['error']) {
             case UPLOAD_ERR_OK:
                 return true;
@@ -68,20 +60,20 @@ class MediaUpload
     {
         $fileInformation = new \finfo(FILEINFO_MIME_TYPE);
         if (false === $ext = array_search(
-            $fileInformation->file($file['tmp_name']),
-            array(
-                'jpg' => 'image/jpeg',
-                'png' => 'image/png',
-                'mp4' => 'video/mp4',
-            ),
-            true
-        )) {
+                $fileInformation->file($file['tmp_name']),
+                array(
+                    'jpg' => 'image/jpeg',
+                    'png' => 'image/png',
+                    'mp4' => 'video/mp4',
+                ),
+                true
+            )) {
             return false;
         }
         return true;
     }
 
-    public function checkDimensions($file) {
-
+    public function checkDimensions($file)
+    {
     }
 }
