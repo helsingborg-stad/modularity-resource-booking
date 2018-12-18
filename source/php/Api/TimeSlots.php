6a4ca96bc4bfc5e2b6ccf198bbe0617bc6711ed1
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2018-12-18 09:33:33 +0100

    Cleaning

--- source/php/Api/TimeSlots.php
+++ source/php/Api/TimeSlots.php
@@ -8,7 +8,7 @@ class TimeSlots
     {
         //Run register rest routes
         add_action('rest_api_init', array($this, 'registerRestRoutes'));
-        //add_action('init', array($this, 'getSlots'));
+
         add_filter('posts_where', array($this, 'postsWhereWildcard'), 2, 10);
     }
 
