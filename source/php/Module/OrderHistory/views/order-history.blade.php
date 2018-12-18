5dd04f6358cf823ad04bbb59fe38d251844e01a8
Jonatan Hanson <jonatan.hanson@hiq.se>
2018-12-18 15:24:11 +0100

    module markup

--- source/php/Module/OrderHistory/views/order-history.blade.php
+++ source/php/Module/OrderHistory/views/order-history.blade.php
@@ -1,1 +1,6 @@
-foobar
+<div class="{{ $classes }}">
+    @if (!$hideTitle && !empty($post_title))
+        <h4 class="box-title">{!! apply_filters('the_title', $post_title) !!}</h4>
+    @endif
+    <div class="modularity-order-history"></div>
+</div>
