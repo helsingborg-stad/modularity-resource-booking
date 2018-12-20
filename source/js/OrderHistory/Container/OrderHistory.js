8cbbcd74195f96be44b3aacea862aca685c7774a
Jonatan Hanson <jonatan.hanson@hiq.se>
2018-12-20 08:38:14 +0100

    Order history component

--- source/js/OrderHistory/Container/OrderHistory.js
+++ source/js/OrderHistory/Container/OrderHistory.js
@@ -1,6 +1,75 @@
+import OrderList from '../Components/OrderList';
+import {getCustomerOrders} from '../../Api/orders';
+import {Pagination} from 'hbg-react';
+
 class OrderHistory extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            error: null,
+            isLoaded: false,
+            items: [],
+            itemValues: [],
+            filteredItems: [],
+            totalPages: 0,
+            currentPage: 1
+        };
+    }
+
+    componentDidMount() {
+        this.getOrders();
+    }
+
+    getOrders = () => {
+        const {perPage} = this.props;
+
+        getCustomerOrders()
+            .then(
+                ({result}) => {
+                    console.log(result);
+
+                    if (!result || Object.keys(result).length === 0) {
+                        this.setState({
+                            error: Error('Could not fetch data from URL.'),
+                            isLoaded: true
+                        });
+                        return;
+                    }
+                    this.setState({
+                            isLoaded: true,
+                            items: result,
+                            filteredItems: result,
+                            totalPages: Math.ceil(result.length / perPage)
+                        },
+                        () => {
+                            this.updateItemList();
+                        });
+                }, ({error}) => {
+                    this.setState({isLoaded: true, error});
+                }
+            );
+    };
+
+    updateItemList = () => {
+        const {items, currentPage} = this.state;
+        const {perPage} = this.props;
+        const begin = ((currentPage - 1) * perPage);
+        const end = begin + perPage;
+
+        this.setState({
+            filteredItems: items.slice(begin, end)
+        });
+    };
+
     render() {
-        return "list";
+        const {filteredItems, error, isLoaded, totalPages, currentPage} = this.state;
+        const {translation} = this.props;
+
+        return (
+            <div>
+                <OrderList items={filteredItems} />
+            </div>
+        );
     }
 }
 
