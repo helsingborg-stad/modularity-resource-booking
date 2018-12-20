c3913528d2b28aadc9d8e68af2ae4194477f3ef7
Jonatan Hanson <jonatan.hanson@hiq.se>
2018-12-20 08:22:52 +0100

    Get orders api helper

---
+++ source/js/Api/orders.js
@@ -0,0 +1,13 @@
+const getCustomerOrders = () => {
+    const {translation, restUrl} = modOrderHistory;
+    const url = restUrl + 'ModularityResourceBooking/v1/MyOrders';
+
+    return fetch(url)
+        .then(res => res.json())
+        .then(
+            (result) => ({result}),
+            (error) => ({error})
+        );
+};
+
+export {getCustomerOrders};
