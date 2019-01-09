b639a307c14c8ceaec8356b1f1654053ac440d48
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2019-01-09 14:31:33 +0100

    Add field for selecting package

---
+++ source/acf-fields/php/mod-booking-form.php
@@ -0,0 +1,49 @@
+<?php 
+
+if (function_exists('acf_add_local_field_group')) {
+    acf_add_local_field_group(array(
+    'key' => 'group_5c35f66a679df',
+    'title' => __('Modularity Resource Booking - Booking Form', 'modularity-resource-booking'),
+    'fields' => array(
+        0 => array(
+            'key' => 'field_5c35f67549fa5',
+            'label' => __('Select product package', 'modularity-resource-booking'),
+            'name' => 'select_package',
+            'type' => 'taxonomy',
+            'instructions' => '',
+            'required' => 1,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'taxonomy' => 'product-package',
+            'field_type' => 'select',
+            'allow_null' => 0,
+            'add_term' => 0,
+            'save_terms' => 0,
+            'load_terms' => 0,
+            'return_format' => 'id',
+            'multiple' => 0,
+        ),
+    ),
+    'location' => array(
+        0 => array(
+            0 => array(
+                'param' => 'post_type',
+                'operator' => '==',
+                'value' => 'mod-resource-booking',
+            ),
+        ),
+    ),
+    'menu_order' => 0,
+    'position' => 'normal',
+    'style' => 'default',
+    'label_placement' => 'top',
+    'instruction_placement' => 'label',
+    'hide_on_screen' => '',
+    'active' => 1,
+    'description' => '',
+));
+}
