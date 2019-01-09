50919627dd987556d770159c9f83c0530f2f5a77
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2019-01-09 13:34:28 +0100

    Email template

--- source/php/Helper/Mail.php
+++ source/php/Helper/Mail.php
@@ -17,6 +17,8 @@ class Mail
     private $_reciver = array();
     private $_subject = null;
     private $_content = null;
+    private $_table = array();
+    private $_headers = array('Content-Type: text/html; charset=UTF-8');
 
     /**
      * Set email reciver
@@ -31,7 +33,7 @@ class Mail
             $this->_reciver[] = $reciver;
             return true;
         }
-        return new WP_Error('malformed_email_adress', __("The email provided was not in a valid format.", 'modularity-resource-booking'));
+        return new \WP_Error('malformed_email_adress', __("The email provided was not in a valid format.", 'modularity-resource-booking'));
     }
 
     /**
@@ -47,7 +49,7 @@ class Mail
             $this->_subject = $subject;
             return true;
         }
-        return new WP_Error('empty_subject', __("The subject cannot be empty.", 'modularity-resource-booking'));
+        return new \WP_Error('empty_subject', __("The subject cannot be empty.", 'modularity-resource-booking'));
     }
 
     /**
@@ -63,7 +65,28 @@ class Mail
             $this->_content = $content;
             return true;
         }
-        return new WP_Error('empty_content', __("The content cannot be empty.", 'modularity-resource-booking'));
+        return new \WP_Error('empty_content', __("The content cannot be empty.", 'modularity-resource-booking'));
+    }
+
+    /**
+     * Set email table
+     *
+     * @param array $table Array containing table rows array(array('heading' => 'Title', 'content' => 'The content'))
+     *
+     * @return true if set, WP_Error if malformed.
+     */
+    public function setTable($table)
+    {
+
+        if (!empty($table) && is_array($table)) {
+            if (!isset($table[0]['heading']) ||!isset($table[0]['content'])) {
+                return new \WP_Error('malformed_table_content', __("Each table array item must contain 'content' and 'title' sub keys.", 'modularity-resource-booking'));
+            }
+            $this->_table = $table;
+            return true;
+        }
+
+        return new \WP_Error('malformed_table', __("The content cannot be empty.", 'modularity-resource-booking'));
     }
 
     /**
@@ -74,18 +97,48 @@ class Mail
     public function dispatch()
     {
         if (empty($this->_reciver)) {
-            return new WP_Error('undefined_reciver', __("Undefined reciver(s).", 'modularity-resource-booking'));
+            return new \WP_Error('undefined_reciver', __("Undefined reciver(s).", 'modularity-resource-booking'));
         }
 
         if (is_null($this->_subject)) {
-            return new WP_Error('undefined_subject', __("Undefined subject line.", 'modularity-resource-booking'));
+            return new \WP_Error('undefined_subject', __("Undefined subject line.", 'modularity-resource-booking'));
         }
 
         if (is_null($this->_content)) {
-            return new WP_Error('undefined_content', __("Undefined content data.", 'modularity-resource-booking'));
+            return new \WP_Error('undefined_content', __("Undefined content data.", 'modularity-resource-booking'));
         }
 
-        return wp_mail(implode(", ", $this->_reciver), $this->_subject, $this->_content);
+        return wp_mail(
+            implode(", ", $this->_reciver),
+            $this->_subject,
+            $this->html($this->_content),
+            $this->_headers
+        );
+    }
+
+    /**
+     * Convert content to HTML
+     *
+     * @return string Html formatted string
+     */
+    public function html($content)
+    {
+        //Create data
+        $data = array(
+            'content' => apply_filters('the_content', $content),
+            'title' => $this->_subject,
+            'table' => $this->_table
+        );
+
+        //Enshure that cache folder exits
+        wp_mkdir_p(trailingslashit(wp_upload_dir()['basedir']) . 'cache/modularity-resource-booking/');
+
+        //Run blade template
+        $blade = new \Philo\Blade\Blade(
+            MODULARITYRESOURCEBOOKING_PATH . "/templates",
+            trailingslashit(wp_upload_dir()['basedir']) . 'cache/modularity-resource-booking/'
+        );
+        return $blade->view()->make('email', $data)->render();
     }
 
     /**
