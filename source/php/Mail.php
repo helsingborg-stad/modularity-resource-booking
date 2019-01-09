aa33e24b0db7adde23e519aa1462b9e5edddee77
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2019-01-09 10:40:14 +0100

    Manager mail

---
+++ source/php/Mail.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace ModularityResourceBooking;
+
+/**
+ * Class Mail
+ *
+ * @category Undefined
+ * @package  ModularityResourceBooking
+ * @author   Sebastian Thulin <sebastian.thulin@helsingborg.se>
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @link     https://helsingborg.se
+ */
+class Mail extends \Helper\Mail
+{
+    /**
+     * Send manager email
+     *
+     * @param string $subject Defines a email subject
+     * @param string $content Defines email content
+     *
+     * @return bool true if sent, false if undefined or malformed email
+     */
+    public static function __construct($subject, $content)
+    {
+        if (!is_wp_error($this->setReciver(get_option('mod_rb_manager_email')))) {
+            $this->setSubject($subject);
+            $this->setContent($content);
+            $this->dispatch();
+            return true;
+        }
+        return false;
+    }
+}
