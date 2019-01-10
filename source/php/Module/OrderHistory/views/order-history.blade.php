0847d6cfea787493562828684571ccf477208fe9
Jonatan Hanson <jonatan.hanson@hiq.se>
2019-01-10 16:47:06 +0100

    test if user is logged in

--- source/php/Module/OrderHistory/views/order-history.blade.php
+++ source/php/Module/OrderHistory/views/order-history.blade.php
@@ -2,5 +2,9 @@
     @if (!$hideTitle && !empty($post_title))
         <h4 class="box-title">{!! apply_filters('the_title', $post_title) !!}</h4>
     @endif
-    <div class="modularity-order-history"></div>
+    @if (is_user_logged_in())
+        <div class="modularity-order-history"></div>
+    @else
+        <div class="gutter"><p><?php echo __('You are not logged in.', 'modularity-resource-booking') ?></p></div>
+    @endif
 </div>
