e05b2e6739242bc54ed355a8e0360321f309d1b3
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2019-01-11 13:09:14 +0100

    Fetch data and render BookingForm Component

--- source/js/BookingForm/Index.js
+++ source/js/BookingForm/Index.js
@@ -1,14 +1,101 @@
 import BookingForm from './Container/BookingForm';
+import { getArticle, getSlots } from '../Api/products';
+import dateFns from 'date-fns';
 
-const domElements = document.getElementsByClassName("modularity-resource-booking-form");
-const {translation} = modResourceBookingForm;
+const {
+    translation,
+    article_type,
+    article_id,
+    user_id,
+    order_nonce
+} = modResourceBookingForm;
 
-for (let i = 0; i < domElements.length; i++) {
-    const element = domElements[i];
-    ReactDOM.render(
-        <BookingForm
-            translation={translation}
-        />,
-        element
-    );
+let avalibleSlots = [],
+    disabledSlots = [];
+
+class App {
+    constructor() {
+        this.articleName = '';
+        this.price = 0;
+        this.avalibleSlots = [];
+        this.disabledSlots = [];
+        this.mediaRequirements = [];
+
+        this.fetchData();
+    }
+
+    /**
+     * Fetch article data and slots
+     * @return {void}
+     */
+    fetchData() {
+        getArticle(article_id, article_type)
+            .then(article => {
+                this.price = article[0].price;
+                this.mediaRequirements = article[0].media_requirements;
+                this.articleName = article[0].title;
+
+                getSlots(article_id, article_type, user_id)
+                    .then(slots => {
+                        this.avalibleSlots = slots.filter(
+                            slot =>
+                                slot.available_stock > 0 &&
+                                !slot.unlimited_stock
+                        );
+
+                        this.disabledSlots = slots.filter(
+                            slot =>
+                                slot.available_stock == 0 ||
+                                slot.unlimited_stock
+                        );
+
+                        this.init();
+                    })
+                    .catch(result => {
+                        console.log('Failed to fetch slots');
+                    });
+            })
+            .catch(result => {
+                console.log('Failed to fetch article');
+            });
+    }
+
+    /**
+     * Convets dates in a slot array to Date object
+     * @param  {array} slots Array containing slots
+     * @return {array}       Slots array with dates converted to Date Object
+     */
+    convertSlotDates(slots) {
+        return slots.map(slot => {
+            slot.start = dateFns.parse(slot.start);
+            slot.stop = dateFns.parse(slot.stop);
+
+            return slot;
+        });
+    }
+
+    /**
+     * Renders React Component
+     * @return {void}
+     */
+    init() {
+        const domElements = document.getElementsByClassName(
+            'modularity-resource-booking-form'
+        );
+        for (let i = 0; i < domElements.length; i++) {
+            const element = domElements[i];
+            ReactDOM.render(
+                <BookingForm
+                    translation={translation}
+                    price={this.price}
+                    avalibleSlots={this.convertSlotDates(this.avalibleSlots)}
+                    disabledSlots={this.convertSlotDates(this.disabledSlots)}
+                    mediaRequirements={this.mediaRequirements}
+                />,
+                element
+            );
+        }
+    }
 }
+
+new App();
