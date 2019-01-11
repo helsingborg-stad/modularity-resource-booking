80269115d0156a517db3130f4195a10113434288
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2019-01-11 13:08:26 +0100

    Add BookingForm PropTypes

--- source/js/BookingForm/Container/BookingForm.js
+++ source/js/BookingForm/Container/BookingForm.js
@@ -1,15 +1,31 @@
 import { Button, Calendar } from 'hbg-react';
+import { getArticle } from '../../Api/products';
+import PropTypes from 'prop-types';
 
 class BookingForm extends React.Component {
+    static propTypes = {
+        price: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+            .isRequired,
+        avalibleSlots: PropTypes.array.isRequired,
+        disabledSlots: PropTypes.array.isRequired,
+        mediaRequirements: PropTypes.array
+    };
+
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            selectedDates: []
+        };
+
+        this.handleClickDate = this.handleClickDate.bind(this);
     }
+
+    handleClickDate(date) {}
 
     render() {
         return (
             <div>
-                <Calendar />
+                <Calendar onClickDate={this.handleClickDate} />
             </div>
         );
     }
