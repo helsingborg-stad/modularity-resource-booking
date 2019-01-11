668eb9410a58c6728c542c49c66ad53f2de28638
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2019-01-11 13:06:37 +0100

    Add helper methods for fetching article and slot data

---
+++ source/js/Api/products.js
@@ -0,0 +1,64 @@
+/**
+ * Fetches article (product or package) data from rest api
+ * @param  {string/int} articleId   Product or Term ID
+ * @param  {string} articleType Defines the article type, can be either "package" or "product"
+ * @return {array}  Article data
+ */
+const getArticle = (articleId, articleType) => {
+    const capitalizeFirstLetter = string => {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    };
+    const { restUrl } = modResourceBookingForm;
+    const url =
+        restUrl +
+        'ModularityResourceBooking/v1/' +
+        capitalizeFirstLetter(articleType) +
+        '/' +
+        articleId;
+
+    return fetch(url)
+        .then(response => {
+            return response.json();
+        })
+        .then(response => {
+            if (response.state == 'error') {
+                throw new Error(response.message);
+            }
+
+            return response;
+        });
+};
+
+/**
+ * Fetches article (product or package) data from rest api
+ * @param  {string/int} articleId   Product or Term ID
+ * @param  {string} articleType Defines the article type, can be either "package" or "product"
+ * @param  {[type]} userId      Wordpress user ID
+ * @return {array}             Array containing slots
+ */
+const getSlots = (articleId, articleType, userId) => {
+    const { restUrl } = modResourceBookingForm;
+    let url = restUrl + 'ModularityResourceBooking/v1/Slots?';
+
+    let params = {
+        articleType: 'type=' + articleType,
+        articleId: '&article_id=' + articleId,
+        userId: '&user_id=' + userId
+    };
+
+    url = url.concat(params.articleType, params.articleId, params.userId);
+
+    return fetch(url)
+        .then(response => {
+            return response.json();
+        })
+        .then(response => {
+            if (response.state == 'error') {
+                throw new Error(response.message);
+            }
+
+            return response;
+        });
+};
+
+export { getArticle, getSlots };
