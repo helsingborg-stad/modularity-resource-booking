c04c31cdcdc75adc60977f133213f763ea73e688
Jonatan Hanson <jonatan.hanson@hiq.se>
2019-01-15 10:31:28 +0100

    Added rest url prop

--- source/js/OrderHistory/Index.js
+++ source/js/OrderHistory/Index.js
@@ -1,12 +1,17 @@
 import OrderHistory from './Container/OrderHistory';
 
 const domElements = document.getElementsByClassName('modularity-order-history');
-const { translation } = modOrderHistory;
+let { translation, restUrl, nonce } = modOrderHistory;
+restUrl += 'ModularityResourceBooking/v1/MyOrders?_wpnonce=' + nonce;
 
 for (let i = 0; i < domElements.length; i++) {
     const element = domElements[i];
     ReactDOM.render(
-        <OrderHistory translation={translation} perPage={10} />,
+        <OrderHistory
+            translation={translation}
+            perPage={10}
+            restUrl={restUrl}
+        />,
         element
     );
 }
