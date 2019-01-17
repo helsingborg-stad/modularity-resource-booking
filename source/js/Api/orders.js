b09e09c523e4b1fe5d83d3885ad69750cda7ec78
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2019-01-17 15:15:09 +0100

    Add API method for creating order

--- source/js/Api/orders.js
+++ source/js/Api/orders.js
@@ -13,4 +13,41 @@ const getCustomerOrders = () => {
         .catch(e => console.log('Request went wrong.'));
 };
 
-export { getCustomerOrders };
+const createOrder = (orders, files) => {
+    const { restUrl, order_nonce } = modResourceBookingForm;
+
+    let url = restUrl + 'ModularityResourceBooking/v1/CreateOrder';
+    let formData = new FormData();
+
+    orders.forEach((order, index) => {
+        formData.append('order_articles[' + index + ']', JSON.stringify(order));
+    });
+
+    files.forEach((media, index) => {
+        formData.append('file[' + index + ']', media.file);
+    });
+
+    formData.append('_wpnonce', order_nonce);
+
+    let options = {
+        method: 'POST',
+        body: formData
+    };
+
+    console.log(order_nonce);
+    console.log(formData);
+
+    return fetch(url, options)
+        .then(response => {
+            return response.json();
+        })
+        .then(response => {
+            if (response.state == 'error') {
+                throw new Error(response.message);
+            }
+
+            return response;
+        });
+};
+
+export { getCustomerOrders, createOrder };
