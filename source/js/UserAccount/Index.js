6e691e003608ed5cbe074f4a3ec67a4dc7040a4f
Jonatan Hanson <jonatan.hanson@hiq.se>
2019-01-25 10:37:59 +0100

    Send module params as data attributes

--- source/js/UserAccount/Index.js
+++ source/js/UserAccount/Index.js
@@ -1,9 +1,19 @@
 import UserAccount from './Container/UserAccount';
 
 const domElements = document.getElementsByClassName('modularity-user-account');
-const { translation, user } = modUserAccount;
+const translation = modUserAccount;
 
 for (let i = 0; i < domElements.length; i++) {
     const element = domElements[i];
-    ReactDOM.render(<UserAccount translation={translation} user={user} />, element);
+    const { user, restUrl, nonce } = element.dataset;
+
+    ReactDOM.render(
+        <UserAccount
+            translation={translation}
+            user={JSON.parse(user)}
+            restUrl={restUrl}
+            nonce={nonce}
+        />,
+        element
+    );
 }
